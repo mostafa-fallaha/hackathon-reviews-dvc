eb9d0cee0e7f857e64f6fe9024a90ca9f831c4ed
mostafa fallaha <moostafafallaha@gmail.com>
2024-08-10 14:09:41 +0300

    removed the nulls from the dataset

--- version_reviews.py
+++ version_reviews.py
@@ -15,7 +15,8 @@ commit_message = args.commit_message
 
 # Run DVC and Git commands
 subprocess.run(["dvc", "add", data_file], check=True)
-subprocess.run(["git", "add", dvc_file, gitignore_file, 'run_versioning_reviews.ps1', 'version_reviews.py', '.gitignore'], check=True)
+subprocess.run(["git", "add", dvc_file, gitignore_file, 'run_versioning_reviews.ps1',
+                'version_reviews.py', '.gitignore', 'reviews_cleaning.ipynb', 'README.md'], check=True)
 subprocess.run(["git", "commit", "-m", commit_message], check=True)
 subprocess.run(["dvc", "push"], check=True)
 subprocess.run(["git", "push"], check=True)
